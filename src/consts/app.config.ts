91e005e25fb89cecc75271944783678831207887
Jan Elznic <jan@elznic.com>
2019-04-23 18:44:50 +0200

    App config TS interfaces

--- src/consts/app.config.ts
+++ src/consts/app.config.ts
@@ -1,4 +1,13 @@
-const AppConfig = {
+interface ApiConfig {
+  readonly baseUrl: string;
+  readonly version: string;
+}
+
+interface AppConfig {
+  readonly api: ApiConfig;
+}
+
+const AppConfig: AppConfig = {
   api: {
     baseUrl: 'http://www.cheapshark.com/api/',
     version: '1.0',
