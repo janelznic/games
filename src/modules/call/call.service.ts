662c8be0ff7ac46a97c5434e213df3648f9e43d2
Jan Elznic <jan@elznic.com>
2019-04-25 00:11:20 +0200

    Day of week bugfixes

--- src/modules/call/call.service.ts
+++ src/modules/call/call.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Call, GetCallsOptions } from './interfaces/call.interface';
 import CallModel from './call.model';
 import { Sequelize } from 'sequelize';
-import * as daysOfWeek from './call.consts';
+import { daysOfWeek } from './call.consts';
 
 @Injectable()
 export class CallService {
@@ -18,9 +18,16 @@ export class CallService {
 
   public getCalls = async (opt?: GetCallsOptions): Promise<any> => {
     try {
+      let where: any = null;
+      if (opt && opt.dayInWeek) {
+        if (daysOfWeek.hasOwnProperty(opt.dayInWeek)) {
+          where = Sequelize.where(Sequelize.fn('WEEKDAY', Sequelize.col('called_at')), daysOfWeek[opt.dayInWeek]);
+        } else {
+          throw new Error(`Specified day ${opt.dayInWeek} is not included in daysOfWeek constant`);
+        }
+      }
       const calls: Call[] = CallModel.findAll({
-        where: opt && opt.dayInWeek ? Sequelize.where(Sequelize.fn('WEEKDAY', Sequelize.col('called_at')), 0) : null,
-        order: [['id', 'ASC']],
+        where, order: [['id', 'ASC']],
       }).then(call => {
         return call;
       });
