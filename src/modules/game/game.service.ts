6b216600eda2010d52512014c74380035c6e3df1
Jan Elznic <jan@elznic.com>
2019-04-24 12:58:09 +0200

    Fetching the cheapest price for specific game

--- src/modules/game/game.service.ts
+++ src/modules/game/game.service.ts
@@ -1,33 +1,74 @@
 import AppConfig from '../../consts/app.config';
 import { Injectable } from '@nestjs/common';
-import Axios, { AxiosResponse } from 'axios';
-import { GameSearchParams } from './interfaces/game.interface';
-import { ICheapSharkResponse } from './interfaces/cheapSharkResponse.interface';
+import Axios from 'axios';
+import { IGame, GameSearchParams } from './interfaces/game.interface';
+import { ICheapSharkGame } from './interfaces/cheapSharkGame.interface';
+import { ICheapSharkDeal } from './interfaces/cheapSharkDeal.interface';
 
 @Injectable()
 export class GameService {
+  apiUrl: string;
+
+  constructor() {
+    this.apiUrl = `${AppConfig.api.baseUrl}/${AppConfig.api.version}`;
+  }
 
   /**
    * Returns list of games
+   * @param searchQuery Name of the game
+   * @param searchParams Query parameters
    */
-  public async getGames(searchQuery: string, searchParams?: GameSearchParams): Promise<ICheapSharkResponse[]> {
-    const gamesList = await this.fetchGameInfo(searchQuery, searchParams);
-    return gamesList.data;
+  public async getGames(searchQuery: string, searchParams?: GameSearchParams): Promise<IGame[]> {
+    const dealList = await this.fetchDeals(searchQuery, searchParams);
+
+    const games = dealList.map(async deal => {
+      const item = await this.fetchDealDetail(deal.dealID);
+
+      return {
+        name: item.gameInfo.name,
+        salePrice: Number(item.gameInfo.salePrice),
+        cheapestPrice: Number(item.cheapestPrice.price),
+        releaseDate: new Date(item.gameInfo.releaseDate * 1000),
+      };
+    });
+
+    return await Promise.all(games);
   }
 
-  private async fetchGameInfo(searchQuery: string, searchParams?: GameSearchParams): Promise<AxiosResponse> {
-    const api = AppConfig.api;
-    const res = await Axios.get(`${api.baseUrl}/${api.version}/deals`, {
+  /**
+   * Fetch deal detail
+   * @param id Deal ID
+   */
+  private async fetchDealDetail(id: string): Promise<ICheapSharkDeal> {
+    const res = await Axios.get(`${this.apiUrl}/deals`, {
+      data: {
+        params: { id },
+      },
+    });
+
+    if (res.status === 200) {
+      return res.data;
+    }
+    throw new Error('Error fetching deal detail');
+  }
+
+  /**
+   * Fetch list of deals
+   * @param searchQuery Name of the game
+   * @param searchParams Query parameters
+   */
+  private async fetchDeals(searchQuery: string, searchParams?: GameSearchParams): Promise<ICheapSharkGame[]> {
+    const res = await Axios.get(`${this.apiUrl}/deals`, {
       data: {
         params: {
           ...searchParams,
           title: encodeURIComponent(searchQuery),
-        }
+        },
       },
     });
     if (res.status === 200) {
-      return res;
+      return res.data;
     }
-    throw new Error('Error fetching games data');
+    throw new Error('Error fetching deals data');
   }
 }
--- src/modules/game/interfaces/cheapSharkGame.interface.ts
+++ src/modules/game/interfaces/cheapSharkGame.interface.ts
@@ -1,4 +1,4 @@
-export interface ICheapSharkResponse {
+export interface ICheapSharkGame {
   readonly internalName: string;
   readonly title: string;
   readonly metacriticLink: string;
